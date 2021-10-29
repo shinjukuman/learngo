017ab8aed485280da0f4c03e7dd488562543c99b
chrisgardner402 <chojh3308@gmail.com>
2021-10-29 23:02:41 +0900

    #1.3 Functions part One

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
-	name := "chris"
-	name = "gardner"
-	fmt.Println(name)
+	// single value return type
+	fmt.Println(multiply(2, 2))
+
+	// multiple value return type
+	totalLength, upperName := lenAndUpper("chris")
+	totalLength2, _ := lenAndUpper("chris")
+	fmt.Println(totalLength, upperName)
+	fmt.Println(totalLength2)
+
+	// multiple arguments
+	repeatMe("dali", "van", "picasso")
 }
