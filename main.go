a560447846aa60003e12e89395bce70fa8c3691a
chrisgardner402 <chojh3308@gmail.com>
2021-10-29 23:14:25 +0900

    #1.4 Functions part Two

--- main.go
+++ main.go
@@ -5,28 +5,14 @@ import (
 	"strings"
 )
 
-func multiply(a, b int) int {
-	return a * b
-}
-
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
-	// single value return type
-	fmt.Println(multiply(2, 2))
-
-	// multiple value return type
-	totalLength, upperName := lenAndUpper("chris")
-	totalLength2, _ := lenAndUpper("chris")
-	fmt.Println(totalLength, upperName)
-	fmt.Println(totalLength2)
-
-	// multiple arguments
-	repeatMe("dali", "van", "picasso")
+	length, uppercase := lenAndUpper("chris")
+	fmt.Println(length, uppercase)
 }
