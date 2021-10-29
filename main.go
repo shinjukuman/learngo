b480f3de1adac6e9c8ed362e89967f2beaab9be0
chrisgardner402 <chojh3308@gmail.com>
2021-10-30 00:40:56 +0900

    #1.8 Pointers

--- main.go
+++ main.go
@@ -4,16 +4,9 @@ import (
 	"fmt"
 )
 
-func canIDrink(age int) bool {
-	switch koreanAge := age + 2; {
-	case koreanAge < 20:
-		return false
-	case koreanAge >= 20:
-		return true
-	}
-	return false
-}
-
 func main() {
-	fmt.Println(canIDrink(18))
+	a := 2
+	b := &a
+	*b = 2000
+	fmt.Println(a)
 }
