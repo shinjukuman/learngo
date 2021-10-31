efdbc227d649baa0295ecf16147bbc0c3a81a319
chrisgardner402 <chojh3308@gmail.com>
2021-10-31 17:18:36 +0900

    #3.6 URLChecker + Go Routines

--- main.go
+++ main.go
@@ -2,24 +2,36 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
 )
 
-func main() {
-	c := make(chan string)
-	people := [5]string{"chris", "gardner", "dali", "van", "picasso"}
-	for _, person := range people {
-		go isSexy(person, c)
-	}
-	fmt.Println("Waiting for messages")
-	fmt.Println("Received this message:", <-c)
-	fmt.Println("Received this message:", <-c)
-	fmt.Println("Received this message:", <-c)
-	fmt.Println("Received this message:", <-c)
-	fmt.Println("Received this message:", <-c)
+type result struct {
+	url    string
+	status string
 }
 
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 5)
-	c <- person + " is sexy"
+func main() {
+	c := make(chan result)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+	}
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+}
+
+func hitURL(url string, c chan<- result) {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	c <- result{url: url, status: status}
 }
