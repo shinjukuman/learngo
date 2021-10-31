313bc54a911e1d8a78929e3b2c26b22c458a9e8a
chrisgardner402 <chojh3308@gmail.com>
2021-10-31 15:11:37 +0900

    #3.1 Slow URLChecker

--- main.go
+++ main.go
@@ -9,18 +9,26 @@ import (
 var errRequestFailed = errors.New("request failed")
 
 func main() {
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
 		"https://www.amazon.com/",
 		"https://www.reddit.com/",
-		"https://www.google.com/",
 		"https://soundcloud.com/",
 		"https://www.facebook.com/",
 		"https://www.instagram.com/",
 	}
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
@@ -29,8 +37,6 @@ func hitURL(url string) error {
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
 		return errRequestFailed
-	} else {
-		fmt.Println(resp.Status)
 	}
 	return nil
 }
