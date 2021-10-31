d5bc959d90e4df914b5013c6a8d88d4e23a92f97
chrisgardner402 <chojh3308@gmail.com>
2021-10-31 16:10:07 +0900

    #3.3 Channels

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 )
 
 func main() {
-	go sexyCount("chris")
-	go sexyCount("gardner")
-	time.Sleep(time.Second * 5)
+	c := make(chan bool)
+	people := [2]string{"chris", "gardner"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
-	}
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	c <- true
 }
