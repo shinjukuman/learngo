9058314ba29b51c93a67275677a7aaa090a8d717
chrisgardner402 <chojh3308@gmail.com>
2021-10-31 20:35:06 +0900

    #4.3 extractJob part Two

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -20,13 +21,17 @@ type extractedJob struct {
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
+	var jobs []extractedJob
 	totalPages := getPages()
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
 	}
+	fmt.Println(jobs)
 }
 
-func getPage(page int) {
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println(pageURL)
 	res, err := http.Get(pageURL)
@@ -38,12 +43,28 @@ func getPage(page int) {
 	checkErr(err)
 
 	doc.Find(".tapItem").Each(func(i int, item *goquery.Selection) {
-		id, _ := item.Attr("data-jk")
-		title := item.Find(".jobTitle>span").Text()
-		companyName := item.Find(".companyName").Text()
-		location := item.Find(".companyLocation").Text()
-		fmt.Println(id, title, companyName, location)
+		job := extractJob(item)
+		jobs = append(jobs, job)
 	})
+	return jobs
+}
+
+func extractJob(item *goquery.Selection) extractedJob {
+	id, _ := item.Attr("data-jk")
+	title := cleanString(item.Find(".jobTitle>span").Text())
+	companyName := cleanString(item.Find(".companyName").Text())
+	location := cleanString(item.Find(".companyLocation").Text())
+	summary := cleanString(item.Find(".job-snippet").Text())
+	return extractedJob{
+		id:          id,
+		title:       title,
+		companyName: companyName,
+		location:    location,
+		summary:     summary}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
 func getPages() int {
